3ad9907d22990a21ad5cbc8d15480f128c56bda6
Aakash Singh <aakkashsingh01@gmail.com>
2025-08-23 14:50:00 +0530

    RideCab Application for ride booking

---
+++ src/main/java/com/myproject/ridecabapp/strategies/StrategyManager.java
@@ -0,0 +1,10 @@
+package com.myproject.ridecabapp.strategies;
+
+
+import org.springframework.stereotype.Component;
+
+
+// manage and return any of the strategies
+@Component
+public class StrategyManager {
+}
